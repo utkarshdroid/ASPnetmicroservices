1c739e7aacb36448861864c68848696920c15644
Utkarsh Sharma <utkarshdroid@gmail.com>
2021-10-16 08:54:31 +0530

    Interface for the Db Context in Catalog API

---
+++ src/Services/Catalog/Catalog.api/Data/ICatalogConext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using MongoDB.Driver;
+using System.Collections.Generic;
+using System.Linq;
+using Catalog.api.Entities;
+using System.Threading.Tasks;
+
+namespace Catalog.api.Data
+{
+    public interface ICatalogConext
+    {
+        IMongoCollection<Product> Products { get; }
+
+    }
+}
